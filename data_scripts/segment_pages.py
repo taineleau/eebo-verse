21467c8d8afa8114392477c63037d5502f2678a4
Danlu Chen <59667474+taineleau@users.noreply.github.com>
2024-07-27 01:11:22 -0700

    Update segment_pages.py

--- data_scripts/segment_pages.py
+++ data_scripts/segment_pages.py
@@ -4,34 +4,49 @@ import re
 from markdownify import markdownify as md
 
 
-def seg_page(path, result_path):
+import os
+from bs4 import BeautifulSoup
+import re
+from markdownify import markdownify as md
+
+
+def seg_page(path, result_path=None):
     with open(path, 'r', encoding='utf-8') as file:
         xml = BeautifulSoup(file, "lxml")
 
     for a in xml.find_all('teiHeader'):
         a.decompose()
 
-    pages = xml.find_all('pb')
+
+    pattern = r'<pb[^>]*>'
+    full_text = xml.prettify()
+    # Find all matches
+    matches = list(re.finditer(pattern, full_text))
 
     new_pages = []
-    last_idx = 0
+    last_end = 0
+    span_name = []
+        
+    for match in matches:
+        left, right = match.span()
+        span_name.append(full_text[left:right])
+        new_pages.append(full_text[last_end:left])
+        last_end = right
+        
+    new_pages.append(full_text[last_end])
 
-    # Convert the BeautifulSoup object to a string only once
-    xml_str = str(xml)
+    # discard the first page
+    new_pages = new_pages[1:]
+    
+    if result_path:
+        with open(result_path, 'w', encoding='utf-8') as wf:
+            for idx, (name, p) in enumerate(zip(span_name, new_pages)):
+                # wf.write(p)
+                wf.write(f"\n\n=====>> {idx} {name}\n")
+                markdown_content = md(p)
+                markdown_content = re.sub(r'\n+', '\n', markdown_content)
+                wf.write(markdown_content)
 
-    for page in pages:
-        current_idx = xml_str.find(str(page))
-        new_pages.append(xml_str[last_idx:current_idx])
-        last_idx = current_idx + len(str(page))
-
-    new_pages.append(xml_str[last_idx:])
-
-    with open(result_path, 'w', encoding='utf-8') as wf:
-        for idx, p in enumerate(new_pages):
-            wf.write(f"\n\n=====>> {idx}\n")
-            markdown_content = md(p)
-            markdown_content = re.sub(r'\n+', '\n', markdown_content)
-            wf.write(markdown_content)
 
 
 def process_all_xml_files(input_dir, output_dir):
