1704dc1ffb805fbf9258bae8d38c6fefb15c9344
tracyqwerty <44852831+tracyqwerty@users.noreply.github.com>
2024-07-29 00:48:31 +0800

    update replacement rule

--- data_scripts/segment_pages_with_replacement.py
+++ data_scripts/segment_pages_with_replacement.py
@@ -2,55 +2,63 @@ from bs4 import BeautifulSoup
 import re
 # from markdownify import markdownify as md
 
-
 def seg_page(path, result_path="result_seg2.md"):
-    xml = BeautifulSoup(open(path).read(), 'lxml')
+    # xml = BeautifulSoup(open(path).read(), 'lxml')
+    with open(path, 'r', encoding='utf-8') as file:
+        xml = file.read()
 
-    # print(xml) # header is removed with hierarchy preserved
-
-    for a in xml.find_all('teiheader'):
-        a.decompose()
-
-    pages = xml.find_all('pb')
+    pattern = r'<pb[^>]*>'
+    full_text = str(xml)#.prettify()
+    matches = list(re.finditer(pattern, full_text))
 
     new_pages = []
-    last_idx = 0
-
-    xml_str = str(xml)
-
-    for page in pages:
-        current_idx = xml_str.find(str(page))
-        if current_idx == -1:
-            continue
-        new_pages.append(xml_str[last_idx:current_idx])
-        last_idx = current_idx + len(str(page))
-
-    new_pages.append(xml_str[last_idx:])
+    last_end = 0
+    span_name = []
+        
+    for i, match in enumerate(matches):
+        left, right = match.span()
+        span_name.append(full_text[left:right])
+        new_pages.append(full_text[last_end:left])
+        last_end = right
+    new_pages.append(full_text[last_end:])
+    new_pages = new_pages[1:]
+    
+    def replace_symbols(match):
+        text = match.group()
+        text = text.replace('•', '[L]')
+        text = text.replace('〈◊〉', '[W]')
+        text = text.replace('〈…〉', '[S]')
+        return text
+    
+    def replace_headers(match):
+        spaces = len(match.group(1))
+        header_level = spaces // 3 - 3 
+        if header_level < 0:
+            print("Warning: header level is less than 0")
+        return f"{'#' * header_level} "
 
     def replace_tags(content):
-        # print(content)
-        # def replace_head_tag(match):
-        #     tag = match.group(0)
-        #     level = tag.count('n')
-        #     print(level)
-        #     content = match.group(1)
-        #     return f'{"#" * level} {content}'
-
-        # content = re.sub(r'<head>(.*?)</head>', replace_head_tag, content)
+        # content = re.sub(r'<teiHeader>.*?</teiHeader>', '', content, flags=re.DOTALL)
         content = re.sub(r'<hi>(.*?)</hi>', r'**\1**', content)
-        content = re.sub(r'<g ref="char:EOLhyphen"/>',
-                         r'- [EOLhyphen]', content)
-        content = re.sub(r'<seg rend="decorInit">(.*?)</seg>',
-                         r'[\1]', content)
-        content = re.sub(r'<opener>(.*?)</opener>', r'```\1```', content)
-        content = re.sub(r'<figure/>', r'figure', content)
-        content = re.sub(r'<desc>〈◊〉</desc>', r'[unread]', content)
-        content = re.sub(r'<desc>•…</desc>', r'[unread]', content)
-        content = re.sub(r'<list>', '', content)
-        content = re.sub(r'</list>', '', content)
-        content = re.sub(r'<item>', '- ', content)
-        content = re.sub(r'</item>', '', content)
-
+        content = re.sub(r'<g(?!ap)([^>]*)>', r'[EOL]', content) # be careful with <gap> and <g>
+        content = re.sub(r'(<desc>.*?<\/desc>)', replace_symbols, content)
+        content = re.sub(r'<figure>(.*?)</figure>', r'fig:\1', content, flags=re.DOTALL)
+        content = re.sub(r'<opener>(.*?)</opener>', r'```\1```', content, flags=re.DOTALL)
+        content = re.sub(r'<seg[^>]*>', r'[DECI]', content) #decorInit
+        
+        # First, remove extra spaces inside the <head> tags
+        content = re.sub(r'<head>(.*?)</head>', lambda m: "<head>" + re.sub(r'\s+', ' ', m.group(1).strip()) + "</head>", content, flags=re.DOTALL)
+        # Then, replace spaces before <head> with appropriate number of #
+        pattern = re.compile(r'^( *)(<head>)', re.MULTILINE)
+        content = re.sub(pattern, replace_headers, content)
+        
+        
+        
+        # content = re.sub(r'<list>', '', content)
+        # content = re.sub(r'</list>', '', content)
+        # content = re.sub(r'<item>', '- ', content)
+        # content = re.sub(r'</item>', '', content)
+        
         content = re.sub(r'<.*?>', '', content)
         return content
 
@@ -63,25 +71,12 @@ def seg_page(path, result_path="result_seg2.md"):
             wf.write(markdown_content)
 
 
-def process_all_xml_files(input_dir, output_dir):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    for file_name in os.listdir(input_dir):
-        if file_name.endswith(".xml"):
-            input_file_path = os.path.join(input_dir, file_name)
-            output_file_path = os.path.join(
-                output_dir, file_name.replace('.xml', '.md'))
-            print(f"Processing {input_file_path} -> {output_file_path}")
-            seg_page(input_file_path, output_file_path)
-
 # input_directory = "/trunk/shared/tcp/all"
 # output_directory = "/trunk3/shared/tracytian/forced_alignment/tcp_with_replacement/"
 # process_all_xml_files(input_directory, output_directory)
 
-
 # Usage example
 # seg_page("/Users/tracyqwerty/Desktop/forced_alignment/A32403.xml")
 seg_page("/Users/tracyqwerty/Desktop/forced_alignment/A19336.xml")
-# seg_page("/Users/tracyqwerty/Desktop/forced_alignment/A19336.xml") # very very large. takes 3 mins+
+# seg_page("/Users/tracyqwerty/Desktop/forced_alignment/A86849.xml")
 # seg_page("/Users/tracyqwerty/Desktop/forced_alignment/N00260.xml")
